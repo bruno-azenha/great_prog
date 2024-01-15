fb9c5ff5f7197f7ea8d2006ad3fc099febc7e98c
Bruno Azenha <azenha.bruno@gmail.com>
2024-01-15 20:58:16 +0800

    upgrade LiveView to 0.18

--- lib/great_prog_web.ex
+++ lib/great_prog_web.ex
@@ -81,8 +81,8 @@ defmodule GreatProgWeb do
       # Use all HTML functionality (forms, tags, etc)
       use Phoenix.HTML
 
-      # Import LiveView helpers (live_render, live_component, live_patch, etc)
-      import Phoenix.LiveView.Helpers
+      # Import Phoenix.Component
+      import Phoenix.Component
 
       # Import basic rendering functionality (render, render_layout, etc)
       import Phoenix.View
